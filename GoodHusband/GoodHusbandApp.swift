a87f2e009d29092281cbbbd83652b39bb3d97374
vinchen <vinchen996@gmail.com>
2024-07-09 15:22:05 +0700

    Initial Commit

---
+++ GoodHusband/GoodHusbandApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoodHusbandApp.swift
+//  GoodHusband
+//
+//  Created by vin chen on 09/07/24.
+//
+
+import SwiftUI
+
+@main
+struct GoodHusbandApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
