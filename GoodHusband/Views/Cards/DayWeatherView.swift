da34a9c1f29295d5ff79a7dec73b30010f829925
Rizki Maulana <rizki.maulana@majoo.id>
2024-07-16 15:34:40 +0700

    Fix resolve conflict

---
+++ GoodHusband/Views/Cards/DayWeatherView.swift
@@ -0,0 +1,43 @@
+//
+//  DayWeatherView.swift
+//  GoodHusband
+//
+//  Created by Rizki Maulana on 16/07/24.
+//
+
+import SwiftUI
+
+struct DayWeather: Identifiable {
+    let id = UUID()
+    let day: String
+    let icon: String
+    let temperature: String
+}
+
+struct DayWeatherView: View {
+    var dayWeather: DayWeather
+    
+    var body: some View {
+        VStack(spacing: 8) {
+            Text(dayWeather.day)
+                .font(.system(size: 15))
+            Image(systemName: dayWeather.icon)
+                .resizable()
+                .frame(width: 24, height: 24)
+                .aspectRatio(contentMode: .fit)
+            Text(dayWeather.temperature)
+                .font(.system(size: 15))
+        }
+        .frame(width: 52, height: 86)
+    }
+}
+
+#Preview {
+    DayWeatherView(
+        dayWeather: DayWeather(
+            day: "Today",
+            icon: "sun.max.fill",
+            temperature: "29°"
+        )
+    )
+}
