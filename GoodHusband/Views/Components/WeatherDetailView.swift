045cf691618a774950b90176d709d828956f22cf
Dyxelta <92355144+Dyxelta@users.noreply.github.com>
2024-07-17 14:51:57 +0700

    feat: integrate data to show weather detail

--- GoodHusband/Views/Components/WeatherDetailView.swift
+++ GoodHusband/Views/Components/WeatherDetailView.swift
@@ -8,17 +8,16 @@
 import SwiftUI
 
 struct WeatherDetailView: View {
-    var imageName: String
-    var value: String
+    @ObservedObject var weatherDetailViewModel: WeatherDetailViewModel
     
     var body: some View {
         VStack {
-            Image(systemName: imageName)
+            Image(systemName: weatherDetailViewModel.imageName.rawValue)
                 .font(.system(size: 18))
                 .foregroundColor(.black)
                 .padding(.bottom, 1)
             
-            Text(value)
+            Text("\(weatherDetailViewModel.value)\(weatherDetailViewModel.imageName == .rainChance || weatherDetailViewModel.imageName == .humidity ? "%" : "")")
                 .font(.system(size: 12))
                 .foregroundColor(.black)
         }
@@ -31,9 +30,9 @@ struct WeatherDetailView: View {
 struct WeatherDetailView_Previews: PreviewProvider {
     static var previews: some View {
         HStack {
-            WeatherDetailView(imageName: "cloud.rain.fill", value: "90%")
-            WeatherDetailView(imageName: "sun.max", value: "UV 5")
-            WeatherDetailView(imageName: "humidity", value: "52%")
+            WeatherDetailView(weatherDetailViewModel: WeatherDetailViewModel(imageName: .rainChance))
+            WeatherDetailView(weatherDetailViewModel: WeatherDetailViewModel(imageName: .uvIndex))
+            WeatherDetailView(weatherDetailViewModel: WeatherDetailViewModel(imageName: .humidity))
         }
         .previewLayout(.sizeThatFits)
     }
